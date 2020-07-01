ad87c4e6262053a9afa8ca2b26dd9345cbf150c8
wankcn <wankcn@icloud.com>
2020-07-02 02:33:30 +0800

    ReadMessage

---
+++ SocketTCP/TCPServer/Message.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Text;
+
+namespace TCPServer
+{
+    public class Message
+    {
+        // 从客户端读取到数据后存入message进行解析，另一方法解析从data中解析
+        private byte[] data = new byte[1024]; // 让最大消息长度小于1024 
+        private int dataLength = 0; // 从0开始存 数组里存了多少个字节的数据
+
+        // 提供访问的方法
+        public byte[] Date => data;
+        public int StartIndex => dataLength;
+
+        // 还剩余的空间
+        public int RemainSize => data.Length - dataLength;
+
+        // 读取到count个字节的数字，让索引加上count
+        public void AddCount(int count)
+        {
+            dataLength += count;
+        }
+
+        /// <summary>
+        /// 解析数据
+        /// </summary>
+        public void ReadMessage()
+        {
+            while (true)
+            {
+                // 小于4数据长度不完整，需要继续接收消息之后进行解析 
+                if (dataLength <= 4)
+                    return;
+                // 先解析长度0-3的4个字节
+                int length = BitConverter.ToInt32(data, 0); // 读取固定4字节
+                // 读取剩余数据 大于等于时说明数据是完整的
+                if (dataLength - 4 >= length)
+                {
+                    Console.WriteLine(dataLength+":"+length);
+                    // 从4号索引开始读取数据
+                    string str = Encoding.UTF8.GetString(data, 4, length);
+                    Console.WriteLine("解析到一条数据：" + str);
+                    // 把后面的数据前移，进行更新
+                    Array.Copy(data, length + 4, data,
+                        0, dataLength - length - 4);
+                    // 更新信息长度 减去已经解析的数据
+                    dataLength -= (length + 4);
+                }
+                else
+                {
+                    break; // 数据不完整，等待新的数据接收
+                }
+            }
+        }
+    }
+}
--- SocketTCP/TCPServer/Program.cs
+++ SocketTCP/TCPServer/Program.cs
@@ -11,76 +11,62 @@ namespace TCPServer
         public static void Main(string[] args)
         {
             StartServerAsync();
-            // 暂停 防止中断
             Console.ReadKey();
         }
 
         // 异步 必须是静态 
         static void StartServerAsync()
         {
-            // 地址类型;socket类型:Dgram(UDP报文) TCP流Stream;协议
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            // 申请端口，绑定局域网ip 10.8.215.46(变动的)  127.0.0.1本机
-            // IPAddress ipAddress= new IPAddress(new byte[]{10,8,215,46}); // 把ip构造成数组传递过去
-            IPAddress ipAddress = IPAddress.Parse("10.8.215.46"); // Parse把一个ip字符串转换成对象传递
-
-            // IpAdress xx.xx.xx.xx IpEedPoint IP地址+终端号
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse("10.8.215.46");
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 4869);
-
-            // 绑定ip和端口号
             serverSocket.Bind(ipEndPoint);
-
-            // 开始监听端口 设置50防止服务器崩溃 设置0表示不限制数量
-            serverSocket.Listen(0); // 传递挂起的连接队列的最大长度 
-
-            // 接收客户端信息 程序会暂停，直到有一个客户端连接过来才会继续向下运行
-            // Socket clientSocket = serverSocket.Accept(); // 接收一个客户端连接 返回一个Socket用来跟客户端进行通信
-            serverSocket.BeginAccept(AcceptCallBack,serverSocket);
-            
+            // 开始监听端口 设置0表示不限制数量
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
         }
+
+        static Message msg = new Message();
 
         // 回调函数 接收到一个客户端连接的时候，要对客户端发起监听
         static void AcceptCallBack(IAsyncResult ar)
         {
             Socket serverSocket = ar.AsyncState as Socket;
             Socket clientSocket = serverSocket.EndAccept(ar);
-            // 向客户端发送一条数据
-            string msg = "Hello client!你好...";
-            byte[] data = Encoding.UTF8.GetBytes(msg); // 按照utf8将一个字符串转换成byte数组
-            clientSocket.Send(data); // Send传递byte数组，需要将字符串转换成byte数组 需要使用支持中文的编码
-            // 持续不断的接收消息 BeginReceive()开始异步接收数据  偏移从0开始存，1024最大数量，事件方法，
-            clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
-            
+            string msgStr = "Hello client!你好...";
+            byte[] data = Encoding.UTF8.GetBytes(msgStr);
+            clientSocket.Send(data);
+            // 偏移从msg.StartIndex开始存，存取的最大数量设置数组的剩余空间msg.RemainSize，事件方法，
+            clientSocket.BeginReceive(msg.Date, msg.StartIndex, msg.RemainSize, SocketFlags.None,
+                ReceiveCallBack, clientSocket);
+
             // 接收完一个客户端之后，重新调用 继续处理下一个客户端连接
-            serverSocket.BeginAccept(AcceptCallBack,serverSocket);
+            serverSocket.BeginAccept(AcceptCallBack, serverSocket);
         }
 
-        // 需要一个byte数组存储数据，静态方便内部访问
-        static byte[] dataBuffer = new byte[1024];
-
-        // 当服务器端接收到一条消息的时候，就会调用事件方法 如果想再去处理，传递数据的时候可以通过null进行传递
         static void ReceiveCallBack(IAsyncResult ar)
         {
-            // 使用try catch处理客户端异常关闭终端
-            // 调用完BeginReceive，表示开始监听客户端数据的传递，当它就收到一条客户端数据的时候才会去调用
-            // 把clientSocket当成一个参数传递到回调方法里面，通过ar.AsyncState来获取到Socket
             Socket clientSocket = null;
             try
             {
                 clientSocket = ar.AsyncState as Socket;
-                int count = clientSocket.EndReceive(ar); // 完成数据的接收  count代表接收了多少个字节的数据
-                // 如果没有接收到数据就关闭 
+                int count = clientSocket.EndReceive(ar);
                 if (count == 0)
                 {
                     clientSocket.Close();
                     return;
                 }
-                // utf-8进行反编码
-                string msg = Encoding.UTF8.GetString(dataBuffer, 0, count);
-                Console.WriteLine("从客户端接收到数据：" + msg);
-                // 完成回调 一直接收数据
-                clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ReceiveCallBack, clientSocket);
+
+                // 读取到count字节以后更新starIndex
+                msg.AddCount(count);
+
+                // *数据解析（循环的解析）
+                msg.ReadMessage();
+
+                // 递归，继续执行服务端接收下一个客户端消息
+                clientSocket.BeginReceive(msg.Date, msg.StartIndex, msg.RemainSize, SocketFlags.None,
+                    ReceiveCallBack, clientSocket);
             }
             catch (Exception e)
             {
@@ -92,6 +78,7 @@ namespace TCPServer
                 }
             }
         }
+
 
         // 同步方式 
         public void StartServerSync()
@@ -107,7 +94,7 @@ namespace TCPServer
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 4869);
 
             // 绑定ip和端口号
-            serverSocket.Bind(ipEndPoint);  
+            serverSocket.Bind(ipEndPoint);
 
             // 开始监听端口 设置50防止服务器崩溃 设置0表示不限制数量
             serverSocket.Listen(0); // 传递挂起的连接队列的最大长度 
