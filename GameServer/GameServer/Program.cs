890c75bf1aab7b6d744cc2bd449c5b7a68e8e99d
wankcn <wankcn@icloud.com>
2020-07-06 21:07:02 +0800

    add-sever-conn

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace GameServer
+﻿using GameServer.Servers;
+namespace GameServer
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            // 启动
+            Server server = new Server("127.0.0.1",4869);
         }
     }
 }
