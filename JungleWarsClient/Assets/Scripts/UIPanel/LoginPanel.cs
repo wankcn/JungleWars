f2bbe7753943085a529652c714ef61906412e2e5
wankcn <wankcn@icloud.com>
2020-07-10 02:37:01 +0800

    fix-showPanel-bug

--- JungleWarsClient/Assets/Scripts/UIPanel/LoginPanel.cs
+++ JungleWarsClient/Assets/Scripts/UIPanel/LoginPanel.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class LoginPanel : BasePanel
 {
+    // 得到close按钮
+    private Button colseButton;
+
     public override void OnEnter()
     {
         base.OnEnter();
+        // 进入时启用自身
+        gameObject.SetActive(true);
         // 先设置成0，通过descale渐变成1 0.3s
         transform.localScale = Vector3.zero;
         transform.DOScale(1, 0.3f);
         // 面板从外面进来 局部位置 结束位置屏幕正中间
         transform.localPosition = new Vector3(1000, 0, 0);
         transform.DOLocalMove(Vector3.zero, 0.3f);
+
+        // 获取关闭按钮 在子物体里，通过transform.Find方法
+        colseButton = transform.Find("CloseButton").GetComponent<Button>();
+        // 注册关闭方法
+        colseButton.onClick.AddListener(OnCloseClick);
+    }
+
+    // 处理close按钮的点击
+    private void OnCloseClick()
+    {
+        // 移除时与进入动画相反
+        transform.DOScale(0, 0.3f);
+        // 移出到目标位置new Vector3(1000, 0, 0)
+        Tweener tweener = transform.DOLocalMove(new Vector3(1000, 0, 0), 0.3f);
+        // 注册回调 播放完动画之后将自身面板pop出去 lamda表达式
+        tweener.OnComplete(() => uiMng.PopPanel());
+    }
+
+    // 禁用
+    public override void OnExit()
+    {
+        base.OnExit();
+        // 将自身禁用调，节约性能
+        gameObject.SetActive(false);
     }
 }
--- JungleWarsClient/Assets/Scripts/UIPanel/StartPanel.cs
+++ JungleWarsClient/Assets/Scripts/UIPanel/StartPanel.cs
@@ -1,23 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StartPanel : BasePanel
 {
+    private Button loginButton;
+    private Animator btnAnimator; // Animator一直运行，想要控制动画，要先把Animator禁用 
+
     public override void OnEnter()
     {
         base.OnEnter();
         // 当Panel被实例化出来时，得到Button组件 根据名字查找
-        Button loginButton = transform.Find("LoginButton").GetComponent<Button>();
+        loginButton = transform.Find("LoginButton").GetComponent<Button>();
+        btnAnimator = loginButton.GetComponent<Animator>();
         // 注册点击事件
         loginButton.onClick.AddListener(OnLoginClik);
     }
-    
+
     // 得到按钮并监听按钮的点击
     private void OnLoginClik()
     {
         // 点击时加载登录面板
         uiMng.PushPanel(UIPanelType.Login);
+    }
+
+    public override void OnPause()
+    {
+        base.OnPause();
+        // 先禁用Animator
+        btnAnimator.enabled = false;
+        // 新的面板弹出时候调用 通过动画隐藏 动画调用完后禁用整个游戏物体 如果只禁用button还是会显示
+        Tweener tweener = loginButton.transform.DOScale(0, 0.3f);
+        tweener.OnComplete(() => loginButton.gameObject.SetActive(false));
+    }
+
+    // 当StartPanel上所以面板都被移除之后，会重新调用
+    public override void OnResume()
+    {
+        base.OnResume();
+        // 先启用游戏物体，再启用游戏物体身上的组件
+        loginButton.gameObject.SetActive(true);
+        // 重新激活时，重新设置为true 动画播放完之后启用
+        Tweener tweener = loginButton.transform.DOScale(1, 0.3f);
+        tweener.OnComplete(() => btnAnimator.enabled = true);
     }
 }
