80b15b5cac14c189b1e031f00bcd85754f4945a9
wankcn <wankcn@icloud.com>
2020-07-20 22:03:55 +0800

    add-panel-animation

--- JungleWarsClient/Assets/Scripts/UIPanel/LoginPanel.cs
+++ JungleWarsClient/Assets/Scripts/UIPanel/LoginPanel.cs
@@ -19,14 +19,7 @@ public class LoginPanel : BasePanel
     public override void OnEnter()
     {
         base.OnEnter();
-        // 进入时启用自身
-        gameObject.SetActive(true);
-        // 先设置成0，通过descale渐变成1 0.3s
-        transform.localScale = Vector3.zero;
-        transform.DOScale(1, 0.3f);
-        // 面板从外面进来 局部位置 结束位置屏幕正中间
-        transform.localPosition = new Vector3(1000, 0, 0);
-        transform.DOLocalMove(Vector3.zero, 0.3f);
+        EnterAnim();
 
         loginRequest = GetComponent<LoginRequest>();
 
@@ -41,26 +34,32 @@ public class LoginPanel : BasePanel
         transform.Find("RegisterButton").GetComponent<Button>().onClick.AddListener(OnRegisterClick);
     }
 
-    // 当界面暂停 新界面push到界面上面的时候调用
+    // 当界面暂停 需要进行隐藏
     public override void OnPause()
     {
-        transform.DOScale(0, 0.3f);
-        // 移出到目标位置new Vector3(1000, 0, 0) 将自身移除
-        Tweener tweener = transform.DOLocalMove(new Vector3(1000, 0, 0), 0.3f);
-        // 注册回调 播放完动画之后将自身面板禁用 节约性能
-        tweener.OnComplete(() => gameObject.SetActive(false));
+        HideAnim();
+    }
+
+    // 面板重新激活的时候
+    public override void OnResume()
+    {
+        // 继续重新显示
+        EnterAnim();
+    }
+
+    // 禁用 
+    public override void OnExit()
+    {
+        // 移除时隐藏
+        HideAnim();
     }
 
     // 处理close按钮的点击
     private void OnCloseClick()
     {
         PlayClikSound(); // 播放点击声音
-        // 移除时与进入动画相反
-        transform.DOScale(0, 0.3f);
-        // 移出到目标位置new Vector3(1000, 0, 0)
-        Tweener tweener = transform.DOLocalMove(new Vector3(1000, 0, 0), 0.3f);
-        // 注册回调 播放完动画之后将自身面板pop出去 lamda表达式
-        tweener.OnComplete(() => uiMng.PopPanel());
+        // 关闭时进行移除面板 触发OnExit()
+        uiMng.PopPanel();
     }
 
     // 登录按钮点击事件
@@ -114,11 +113,21 @@ public class LoginPanel : BasePanel
         uiMng.PushPanel(UIPanelType.Register);
     }
 
-    // 禁用 
-    public override void OnExit()
+    private void EnterAnim()
     {
-        base.OnExit();
-        // 将自身禁用调，节约性能
-        gameObject.SetActive(false);
+        // 进入时启用自身
+        gameObject.SetActive(true);
+        // 先设置成0，通过descale渐变成1 0.3s
+        transform.localScale = Vector3.zero;
+        transform.DOScale(1, 0.3f);
+        // 面板从外面进来 局部位置 结束位置屏幕正中间
+        transform.localPosition = new Vector3(1000, 0, 0);
+        transform.DOLocalMove(Vector3.zero, 0.3f);
+    }
+
+    private void HideAnim()
+    {
+        transform.DOScale(0, 0.3f);
+        transform.DOLocalMoveX(1000, 0.3f).OnComplete(() => gameObject.SetActive(false));
     }
 }
