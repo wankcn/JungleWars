31431644806d1ea432ee763f2da52630ddc3368f
wankcn <wankcn@icloud.com>
2020-07-20 21:38:25 +0800

    closebutton-hideAnim

--- JungleWarsClient/Assets/Scripts/UIPanel/RoomListPanel.cs
+++ JungleWarsClient/Assets/Scripts/UIPanel/RoomListPanel.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomListPanel : BasePanel
 {
@@ -15,12 +16,28 @@ public class RoomListPanel : BasePanel
     {
         battleRes = transform.Find("BattleRes").GetComponent<RectTransform>();
         roomList = transform.Find("RoomList").GetComponent<RectTransform>();
+        transform.Find("RoomList/CloseButton").GetComponent<Button>().onClick.AddListener(OnCloseClick);
         EnterAnim();
+    }
+
+    public override void OnExit()
+    {
+        HideAnim(); // 退出时进行隐藏
+    }
+
+    // 注册给Close按钮
+    private void OnCloseClick()
+    {
+        PlayClikSound();
+        // 将自身弹出去
+        uiMng.PopPanel();
+        
     }
 
     // 进入的动画
     private void EnterAnim()
     {
+        gameObject.SetActive(true); // 启用再播放
         battleRes.localPosition = new Vector3(-1000, 0);
         battleRes.DOLocalMoveX(-563, 0.5f);
 
@@ -31,5 +48,8 @@ public class RoomListPanel : BasePanel
     // 隐藏的动画
     private void HideAnim()
     {
+        battleRes.DOLocalMoveX(-1000, 0.5f);
+        // 当前游戏物体进行隐藏
+        roomList.DOLocalMoveX(1000, 0.5f).OnComplete(() => gameObject.SetActive(false));
     }
 }
