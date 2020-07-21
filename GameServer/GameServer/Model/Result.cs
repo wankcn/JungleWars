ae3bc99ccb7dc390a2b2a6ef6857a06d43d2f14b
wankcn <wankcn@icloud.com>
2020-07-21 21:35:39 +0800

    add-result

---
+++ GameServer/GameServer/Model/Result.cs
@@ -0,0 +1,17 @@
+namespace GameServer.Model
+{
+    public class Result
+    {
+        public Result(int id, int userId,int totalCount,int winCount)
+        {
+            this.Id = id;
+            this.UserId = userId;
+            this.TotalCount = totalCount;
+            this.WinCount = winCount;
+        }
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TotalCount { get; set; }
+        public int WinCount { get; set; }
+    }
+}
