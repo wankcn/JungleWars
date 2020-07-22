7d4878e84f9e1b4c9831fea3bb07dabac440b365
wankcn <wankcn@icloud.com>
2020-07-22 23:44:12 +0800

    modify-playerMng

--- JungleWarsClient/Assets/Scripts/Manager/PlayerManager.cs
+++ JungleWarsClient/Assets/Scripts/Manager/PlayerManager.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections;
+﻿// 角色初始化和管理
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : BaseManager
 {
+    // 保存用户数据
+    private UserData userData;
+
     public PlayerManager(GameFacade facade) : base(facade)
     {
+    }
+
+    public UserData UserData
+    {
+        // 提供get方法访问数据
+        get { return userData; }
+        set => userData = value;
     }
 }
