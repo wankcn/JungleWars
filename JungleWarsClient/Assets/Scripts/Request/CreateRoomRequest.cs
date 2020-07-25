4eeaed3fd56d4ff77f1851692014ce355443784a
wankcn <wankcn@icloud.com>
2020-07-25 22:48:56 +0800

    create-roomrequest-script

---
+++ JungleWarsClient/Assets/Scripts/Request/CreateRoomRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+
+public class CreateRoomRequest : BaseRequest
+{
+    // 在Awake里面设置requestCode和ActionCode
+    public override void Awake()
+    {
+        requestCode = RequestCode.Room;
+        actionCode = ActionCode.CreateRoom;
+        base.Awake();
+    }
+    
+    // 用来发起请求
+    public override void SendRequest()
+    {
+        // 传递r防止数据解析的时候出现问题
+        base.SendRequest("r");
+    }
+
+    // 监听回调
+    public override void OnResponse(string data)
+    {
+        base.OnResponse(data);
+    }
+}
